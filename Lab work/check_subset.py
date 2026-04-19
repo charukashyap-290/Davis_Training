d0917fa151466cbd85cccc285a70d14d00568e2b
charukashyap-290 <charukashyap290@gmail.com>
2026-04-19 13:08:24 +0530

    Create check_subset.py

---
+++ Lab work/check_subset.py
@@ -0,0 +1,12 @@
+'''Question 8: Check Subset
+Problem Statement:
+Check whether one set is a subset of another.
+Input:
+Set1 = {1, 2}, Set2 = {1, 2, 3, 4}
+Output:
+True'''
+
+set1 = {1, 2}
+set2 = {1, 2, 3, 4}
+
+print(set1.issubset(set2))
