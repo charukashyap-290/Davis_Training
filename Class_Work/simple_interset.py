29f28cceb5e1da8ea29efb1b2f1f7ed695043640
charukashyap-290 <charukashyap290@gmail.com>
2026-04-19 12:57:22 +0530

    Update simple_interset.py

--- Class_Work/simple_interset.py
+++ Class_Work/simple_interset.py
@@ -1,1 +1,6 @@
-
+def calculate(p,r,t):
+    return((p*r*t)/100)
+principal = float(input("Enter principal(in rs):"))
+rate = float(input("Enter rate of interest(in %):"))
+time = int(input("Enter time (in year):"))
+print("Simple interest: Rs", calculate (principal,rate,time))
